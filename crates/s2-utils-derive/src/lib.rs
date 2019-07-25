3757b7aff6e7fc0a806e2feb29139ae7222a0c18
Flux Xu <fluxxu@gmail.com>
2019-07-25 01:49:35 -0400

    fix: fix StructMapper attr name.

--- crates/s2-utils-derive/src/lib.rs
+++ crates/s2-utils-derive/src/lib.rs
@@ -14,17 +14,17 @@ mod has_item_key;
 mod str_enum;
 mod struct_mapper;
 
-#[proc_macro_derive(StructMapper, attributes(map_from))]
+#[proc_macro_derive(StructMapper, attributes(struct_mapper))]
 pub fn derive_struct_mapper(input: TokenStream) -> TokenStream {
-  struct_mapper::derive(input)
+    struct_mapper::derive(input)
 }
 
 #[proc_macro_derive(StrEnum, attributes(str_enum_impl))]
 pub fn derive_str_enum(input: TokenStream) -> TokenStream {
-  str_enum::derive(input)
+    str_enum::derive(input)
 }
 
 #[proc_macro_derive(HasItemKey, attributes(has_item_key))]
 pub fn derive_has_item_key(input: TokenStream) -> TokenStream {
-  has_item_key::derive(input)
+    has_item_key::derive(input)
 }
